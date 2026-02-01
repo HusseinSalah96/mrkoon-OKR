251acb626b72ba51b4b21384a49cdf788a086ea4
HusseinSalah96 <hussein_salah@mrkoonapp.com>
2026-02-01 17:46:54 +0200

    increase version 3

--- apps/backend/src/serverless.ts
+++ apps/backend/src/serverless.ts
@@ -6,6 +6,17 @@ import { join } from 'path';
 let app: NestExpressApplication;
 
 export default async function handler(req: any, res: any) {
+    // Manually handle CORS
+    res.setHeader('Access-Control-Allow-Origin', 'https://mrkoon-okr-frontend.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS,PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     if (!app) {
         app = await NestFactory.create<NestExpressApplication>(AppModule);
         app.enableCors({
